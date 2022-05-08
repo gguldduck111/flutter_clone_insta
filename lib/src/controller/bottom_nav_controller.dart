3070453916d7c369b2a616a556732299126acfcd
gguldduck111 <46213594+gguldduck111@users.noreply.github.com>
2022-05-08 14:19:06 +0900

    Initial commit

---
+++ lib/src/controller/bottom_nav_controller.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_clone_insta/src/components/message_popup.dart';
+import 'package:flutter_clone_insta/src/pages/upload.dart';
+import 'package:get/state_manager.dart';
+import 'package:get/get.dart';
+
+enum PageName { HOME, SEARCH, UPLOAD, ACTIVITY, MYPAGE }
+
+class BottomNavController extends GetxController {
+  RxInt pageIndex = 0.obs;
+  List<int> bottomHistory = [0];
+
+  void changeBottomNav(int value, {bool hasGesture = true}) {
+    var page = PageName.values[value];
+    switch (page) {
+      case PageName.UPLOAD:
+        Get.to(() => const Upload());
+        break;
+      case PageName.HOME:
+      case PageName.SEARCH:
+      case PageName.ACTIVITY:
+      case PageName.MYPAGE:
+        _changePage(value, hasGesture: hasGesture);
+        break;
+      default:
+    }
+  }
+
+  void _changePage(int value, {bool hasGesture = true}) {
+    pageIndex(value);
+    if (!hasGesture) return;
+    if (bottomHistory.last != value) {
+      bottomHistory.add(value);
+    }
+  }
+
+  Future<bool> willPopAction() async {
+    if (bottomHistory.length == 1) {
+      showDialog(
+          context: Get.context!,
+          builder: (context) => MessagePopup(
+                title: '시스템',
+                message: '종료하시겠습니까?',
+                okCallback: () {
+                  exit(0);
+                },
+                cancelCallback: Get.back,
+              ));
+      return true;
+    } else {
+      bottomHistory.removeLast();
+      changeBottomNav(bottomHistory.last, hasGesture: false);
+      return false;
+    }
+  }
+}
