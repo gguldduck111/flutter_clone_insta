aea3327d87d1c0e6127f625ee8de1426f51c326d
gguldduck111 <46213594+gguldduck111@users.noreply.github.com>
2022-05-08 14:21:05 +0900

    [플러터] 인스타그램 클론 코딩 #2 - BottomNavigation + History

--- lib/src/binding/init_bindings.dart
+++ lib/src/binding/init_bindings.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_clone_insta/src/controller/bottom_nav_controller.dart';
 import 'package:get/instance_manager.dart';
 
-class InitBinding extends Bindings{
+class InitBinding extends Bindings {
   @override
-  void dependencies(){
-    Get.put(BottomNavController(),permanent: true);
+  void dependencies() {
+    Get.put(BottomNavController(), permanent: true);
   }
 }
